10a7474e07023279c04ca2936f8e01110a8388d0
Sofia Noor Rafa <sofiautilitarian@email.com>
2025-08-06 11:33:36 +0600

    creating hamburger menu

--- shirushidemo/src/App.vue
+++ shirushidemo/src/App.vue
@@ -1,10 +1,41 @@
 <template>
   <v-app>
+    <!-- Sidebar component -->
+    <SideBar />
+
+    <!-- Main content -->
     <v-main>
-      <div></div>
+      <v-container>
+        <v-card class="pa-4">
+          <v-navigation-drawer
+            v-model="drawer"
+            expand-on-hover
+            rail
+            temporary
+          >
+            <v-list>
+              <v-list-item>
+                Hello from drawer!
+              </v-list-item>
+            </v-list>
+          </v-navigation-drawer>
+        </v-card>
+      </v-container>
     </v-main>
   </v-app>
 </template>
 
 <script>
+import SideBar from './components/SideBar.vue';
+
+export default {
+  components: {
+    SideBar,
+  },
+  data() {
+    return {
+      drawer: false,
+    };
+  },
+};
 </script>
---
+++ shirushidemo/src/components/SideBar.vue
@@ -0,0 +1,24 @@
+<template>
+  <v-app>
+    <v-navigation-drawer v-model="drawer" temporary>
+      Hello
+    </v-navigation-drawer>
+
+    <v-app-bar app>
+      <v-app-bar-nav-icon @click="drawer = !drawer" />
+      <v-toolbar-title>My App</v-toolbar-title>
+    </v-app-bar>
+
+    <v-main>
+      <v-container>
+        Main content
+      </v-container>
+    </v-main>
+  </v-app>
+</template>
+
+<script setup>
+import { ref } from 'vue'
+
+const drawer = ref(false)
+</script>
